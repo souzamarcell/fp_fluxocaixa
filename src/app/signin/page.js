537c6d12c5e8b4ebc12fe4f56cb80e621b76ca0e
Marcell Souza <marcell.ramos@gmail.com>
2024-10-21 12:09:27 -0400

    party 1.0.1

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -2,7 +2,7 @@
 import { toast } from 'react-toastify'
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
-import logo from '../image/prolam.png'
+import logo from '../image/fabrica.png'
 import { ValidateLogin } from '../utility/validateLogin'
 import { useRouter } from 'next/navigation'
 
